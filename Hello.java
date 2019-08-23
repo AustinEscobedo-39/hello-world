49cd1745666714a92f6363e994511579686cb20c
AustinEscobedo-39 <54456308+AustinEscobedo-39@users.noreply.github.com>
2019-08-23 17:40:10 -0400

    Add files via upload
    Hello world lab to be completed for cmsc 255 001

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*********************************************************
+ * Hello
+ *********************************************************
+ * Hello Lab 1 
+ * _______________________________________________________
+ * Austin Escobedo
+ * 8/23/2019
+ * CMSC 255 001
+ *********************************************************/
+ 
+ public class Hello {
+  public static void printHeading() {
+   System.out.println("Austin Escobedo");
+   System.out.println("Hello");
+   System.out.println("Hello Lab 1");
+   System.out.println("8/23/2019");
+   System.out.println("CMSC 255 001");
+   System.out.println();
+  }
+  
+  public static void main(String [] args) {
+   printHeading();
+   System.out.println("Hello");
+  }
+ }
